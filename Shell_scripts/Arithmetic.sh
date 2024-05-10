7d6cc6acaa463022ec67ae6d693d27885e4cfd65
Abhishek Kumar <Abhishekundefeated0687@gmail.com>
2024-05-11 01:04:10 +0530

    Arithmetic

---
+++ Shell_scripts/Arithmetic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Arithmetic Problem
+read -p" Enter first number " num
+read -p" Enter second number " num2
+echo $(($num + $num2))
+echo "Now finding multiplication of two numbers"
+echo $(($num * $num2))
